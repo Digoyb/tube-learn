09542e310733496592060627ecc7ebf7f0ccf72e
rodrigo.spinelli <digoh_straight@hotmail.com>
2019-09-14 17:24:50 -0300

    criando classlib domain

---
+++ TubeLearn.Domain/entities/Canal.cs
@@ -0,0 +1,7 @@
+namespace TubeLearn.Domain.entities
+{
+    public class Canal
+    {
+        public int MyProperty { get; set; }
+    }
+}
---
+++ TubeLearn.Domain/entities/Video.cs
@@ -0,0 +1,7 @@
+namespace TubeLearn.Domain.entities
+{
+    public class Video
+    {
+        
+    }
+}
